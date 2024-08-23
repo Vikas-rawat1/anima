e2e499b18cea71ba8fa71bbee84ca75f9fe05b3a
vikas <rawatvikas689@gmail.com>
2024-08-23 19:55:27 +0530

    commit

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ textAnimate.from("#para h1", {
   y: -30,adadffasdf
   opacity: 0.3,adfadssdffaasdfsdf
   duration: 1,aasdasdfasdfasasddf
-  delay: 0.8,adssdfasdfa
+  delay: 0.8,adssdfasdf
 
   scrollTrigger: {
     trigger: "#paradfsasfd ",ad
